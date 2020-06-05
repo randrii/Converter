af944e7e060e3abb605f130ede1f7c063112afd7
andrii.rybka <andrii.rybka@avenga.com>
2020-06-05 13:03:11 +0300

    applied changes, added LocalDateTime

--- src/main/java/com/rybka/config/FileUtils.java
+++ src/main/java/com/rybka/config/FileUtils.java
@@ -2,18 +2,17 @@ package com.rybka.config;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class FileUtils {
 
     public static final String FILE_NAME_PATTERN = "yyyy-MM-dd_hh-mm-ss";
     public static final String CSV_SUFFIX = ".csv";
     public static final String JSON_SUFFIX = ".json";
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat(FILE_NAME_PATTERN);
 
     public static String generateFileName(String fileSuffix) {
-        Date date = Calendar.getInstance().getTime();
-        DateFormat dateFormat = new SimpleDateFormat(FILE_NAME_PATTERN);
-        return dateFormat.format(date) + fileSuffix;
+        LocalDateTime DATE_TIME = LocalDateTime.now();
+        return DATE_FORMAT.format(DATE_TIME) + fileSuffix;
     }
 }
