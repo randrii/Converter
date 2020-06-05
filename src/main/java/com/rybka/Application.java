bd7672eb6a03985e5a08a49935491bf7937b075c
andrii.rybka <andrii.rybka@avenga.com>
2020-06-05 16:51:16 +0300

    export from prop file, handled some issues

--- src/main/java/com/rybka/Application.java
+++ src/main/java/com/rybka/Application.java
@@ -1,12 +1,16 @@
 package com.rybka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.rybka.config.FileUtils;
 import com.rybka.config.PropertyInfo;
 import com.rybka.dao.CurrencyHistoryDAO;
-import com.rybka.service.connector.BaseCurrencyExchangeConnector;
 import com.rybka.service.connector.ExchangeRateConnector;
 import com.rybka.service.exchange.ExchangeService;
 import com.rybka.service.connector.PrimeExchangeRateConnector;
+import com.rybka.service.export.CSVExportService;
+import com.rybka.service.export.ConsoleExportService;
+import com.rybka.service.export.JSONExportService;
 import com.rybka.view.ExchangeView;
 import coresearch.cvurl.io.request.CVurl;
 
@@ -30,10 +34,25 @@ public class Application {
         var reader = new PropertyReader(PropertyInfo.PROPERTY_FILE_NAME).getProperties();
         var connector = exchangeSourceMap.get(reader.getProperty(PropertyInfo.PROPERTY_EXCHANGE_SOURCE));
 
+        var exportFolder = reader.getProperty(PropertyInfo.PROPERTY_EXPORT_FOLDER);
+        var exportFileName = FileUtils.generateFileName(reader.getProperty(PropertyInfo.PROPERTY_EXPORT_TYPE));
+
+        var consoleExportService = new ConsoleExportService();
+        var csvExportService = new CSVExportService(new CsvMapper(), exportFolder, exportFileName);
+        var jsonExportService = new JSONExportService(objectMapper, exportFolder, exportFileName);
+
+        var exportConfigMap = Map.of(
+                PropertyInfo.PROPERTY_EXPORT_CONSOLE, consoleExportService,
+                PropertyInfo.PROPERTY_EXPORT_CSV, csvExportService,
+                PropertyInfo.PROPERTY_EXPORT_JSON, jsonExportService);
+
+        var exportService = exportConfigMap.get(reader.getProperty(PropertyInfo.PROPERTY_EXPORT_TYPE));
+
         ExchangeView view = new ExchangeView(
                 new Scanner(System.in),
                 new ExchangeService(connector),
-                new CurrencyHistoryDAO()
+                new CurrencyHistoryDAO(),
+                exportService
         );
 
         while (true) {
--- src/main/java/com/rybka/config/PropertyInfo.java
+++ src/main/java/com/rybka/config/PropertyInfo.java
@@ -5,4 +5,10 @@ public final class PropertyInfo {
     public static final String PROPERTY_EXCHANGE_SOURCE = "exchange.source";
     public static final String PROPERTY_EXCHANGE_KEY = "exchange";
     public static final String PROPERTY_PRIME_EXCHANGE_KEY = "prime_exchange";
+
+    public static final String PROPERTY_EXPORT_FOLDER = "export.folder";
+    public static final String PROPERTY_EXPORT_TYPE = "export.type";
+    public static final String PROPERTY_EXPORT_CONSOLE = "console";
+    public static final String PROPERTY_EXPORT_CSV = "csv";
+    public static final String PROPERTY_EXPORT_JSON = "json";
 }
