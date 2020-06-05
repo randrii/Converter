4c247d299a0fd4f176ed6d9ac976716e4c530626
andrii.rybka <andrii.rybka@avenga.com>
2020-06-05 18:21:49 +0300

    applied changes, added enums, maps throws exception, refactoring

--- src/main/java/com/rybka/Application.java
+++ src/main/java/com/rybka/Application.java
@@ -2,9 +2,12 @@ package com.rybka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.rybka.config.ExchangeSource;
+import com.rybka.config.ExportType;
 import com.rybka.config.FileUtils;
 import com.rybka.config.PropertyInfo;
 import com.rybka.dao.CurrencyHistoryDAO;
+import com.rybka.exception.InvalidPropertyException;
 import com.rybka.service.connector.ExchangeRateConnector;
 import com.rybka.service.exchange.ExchangeService;
 import com.rybka.service.connector.PrimeExchangeRateConnector;
@@ -28,11 +31,15 @@ public class Application {
         var exchangeRateConnectorConnector = new ExchangeRateConnector(new CVurl());
 
         var exchangeSourceMap = Map.of(
-                PropertyInfo.PROPERTY_EXCHANGE_KEY, exchangeRateConnectorConnector,
-                PropertyInfo.PROPERTY_PRIME_EXCHANGE_KEY, primeExchangeRateConnector);
+                ExchangeSource.EXCHANGE.getSource(), exchangeRateConnectorConnector,
+                ExchangeSource.PRIME_EXCHANGE.getSource(), primeExchangeRateConnector);
 
         var reader = new PropertyReader(PropertyInfo.PROPERTY_FILE_NAME).getProperties();
-        var connector = exchangeSourceMap.get(reader.getProperty(PropertyInfo.PROPERTY_EXCHANGE_SOURCE));
+        var connector = exchangeSourceMap.entrySet().stream()
+                .filter(item -> reader.getProperty(PropertyInfo.PROPERTY_EXCHANGE_SOURCE).equals(item.getKey()))
+                .findAny()
+                .orElseThrow(() -> new InvalidPropertyException("Cannot find specified source. Try using another one."))
+                .getValue();
 
         var exportFolder = reader.getProperty(PropertyInfo.PROPERTY_EXPORT_FOLDER);
         var exportFileName = FileUtils.generateFileName(reader.getProperty(PropertyInfo.PROPERTY_EXPORT_TYPE));
@@ -42,11 +49,15 @@ public class Application {
         var jsonExportService = new JSONExportService(objectMapper, exportFolder, exportFileName);
 
         var exportConfigMap = Map.of(
-                PropertyInfo.PROPERTY_EXPORT_CONSOLE, consoleExportService,
-                PropertyInfo.PROPERTY_EXPORT_CSV, csvExportService,
-                PropertyInfo.PROPERTY_EXPORT_JSON, jsonExportService);
+                ExportType.CONSOLE.getType(), consoleExportService,
+                ExportType.CSV.getType(), csvExportService,
+                ExportType.JSON.getType(), jsonExportService);
 
-        var exportService = exportConfigMap.get(reader.getProperty(PropertyInfo.PROPERTY_EXPORT_TYPE));
+        var exportService = exportConfigMap.entrySet().stream()
+                .filter(item -> reader.getProperty(PropertyInfo.PROPERTY_EXPORT_TYPE).equals(item.getKey()))
+                .findFirst()
+                .orElseThrow(() -> new InvalidPropertyException("Unable to find specified export type or it isn't supported."))
+                .getValue();
 
         ExchangeView view = new ExchangeView(
                 new Scanner(System.in),
