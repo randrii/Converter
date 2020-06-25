2a9c1d7a8976a401d4ddf7a2064967e7f64a2fc8
ytuzhanskyi <yaroslav.tuzhanskyi@avenga.com>
2020-06-25 11:39:14 +0300

    * fix

--- src/main/java/com/rybka/Application.java
+++ src/main/java/com/rybka/Application.java
@@ -1,27 +1,13 @@
 package com.rybka;
 
-import com.rybka.configuration.ApplicationConfiguration;
-import com.rybka.configuration.PersistenceJPAConfiguration;
-import com.rybka.view.ExchangeView;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-        var context = new AnnotationConfigApplicationContext(
-                ApplicationConfiguration.class,
-                PersistenceJPAConfiguration.class);
-
-        var view = (ExchangeView) context.getBean("exchangeView");
-
-        while (true) {
-            view.showView();
-        }
     }
 
 }
